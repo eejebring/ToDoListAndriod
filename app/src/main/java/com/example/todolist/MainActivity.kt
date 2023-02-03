faf77a98a4b01b889b62724309700236b516cf1a
eejebring <dev@ejebring.com>
2023-02-03 20:07:34 +0100

    Made a navigation link. Tinkered with the checkbox, still un functional.

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -7,20 +7,25 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.todolist.ui.theme.ToDoCompose
 import com.example.todolist.ui.theme.ToDoListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ToDoListTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    layout()
+                    start()
                 }
             }
         }
@@ -28,32 +33,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    var col = Color.Red
-    Text(text = "Hello $name!", color = col)
+fun Heading(text: String) {
+    Text(text = text,
+        style = MaterialTheme.typography.h2
+        )
 }
 
 @Composable
-fun layout () {
-    val listOfToDos = remember { mutableListOf<ToDoItem>(ToDoItem("Do the thing")) }
-    Column  {
+fun start() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "layout") {
+        composable("layout") { layout(navController) }
+        composable("newToDo") { Heading("hi") }
+    }
+}
+
+@Composable
+fun layout(navController: NavHostController) {
+    val listOfToDos = remember {
+        mutableListOf(ToDoItem("Do the thing"), ToDoItem("the other thing"))
+    }
+
+    Column (horizontalAlignment = Alignment.CenterHorizontally) {
         Row {
-            Greeting("Android")
-            Greeting("Hello World!")
+            Heading("Your To-do list")
         }
-        Divider(color = Color.Black)
         Row {
-            Greeting(listOfToDos.value.getTitle())
+            Button(
+                onClick = { navController.navigate("newToDo")},
+
+            ) {
+                Text("New: to-do")
+            }
         }
-        Divider()
-        ToDoCompose(listOfToDos.value)
+        for (toDo in listOfToDos) {
+            ToDoCompose(toDo)
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val navController = rememberNavController()
     ToDoListTheme {
-        layout()
+        layout(navController)
     }
 }
--- app/src/main/java/com/example/todolist/ToDoCompose.kt
+++ app/src/main/java/com/example/todolist/ToDoCompose.kt
@@ -1,27 +1,39 @@
 package com.example.todolist.ui.theme
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Checkbox
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.example.todolist.ToDoItem
 
 @Composable
 fun ToDoCompose(item: ToDoItem) {
-    Box (Modifier.fillMaxWidth()) {
+    val toDo = remember { mutableStateOf(ToDoItem("the other thing")) }
+    var num = remember { mutableStateOf(0) }
+    Divider()
+    Box (Modifier
+        .fillMaxWidth()
+        .clickable { num.value++ }
+    ) {
         Checkbox(
-            item.isDone,
-            { item.toggleDone() },
+            toDo.value.isDone,
+            { toDo.value.toggleDone() },
             Modifier.align(Alignment.CenterStart)
         )
-        Text(item.getTitle(),
+        Text(toDo.value.getTitle() + num.value,
             Modifier.align(Alignment.Center)
         )
         Icon(
