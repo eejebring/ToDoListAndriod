663db390feb684f1c49ffabae86d1419cc9dd624
eejebring <dev@ejebring.com>
2023-02-21 15:50:11 +0100

    Validation added

--- app/src/main/java/com/example/todolist/ToDoEdit.kt
+++ app/src/main/java/com/example/todolist/ToDoEdit.kt
@@ -1,31 +1,65 @@
 package com.example.todolist
 
+import android.text.style.BackgroundColorSpan
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Colors
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
 
+val MIN_TITLE_LENGTH = 3
+val MAX_TITLE_LENGTH = 20
+val MAX_DESCRIPTION_LENGTH = 255
+
+private val validationViolations = mutableStateListOf<String>()
 @Composable
 fun ToDoEdit(toDoItem: ToDoItem, navController: NavController, saveFunction: (ToDoItem) -> Unit) {
     Row (verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxHeight()) {
         Column (horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
+            for (violation in validationViolations) {
+                Text(violation, modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.Red),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center
+                )
+            }
             Text("Title:")
             TextField(toDoItem.title.value, { value -> toDoItem.title.value = value })
-            Row (modifier = Modifier.padding(10.dp)) {
-                Text("Description:")
-                TextField(toDoItem.content.value, { value -> toDoItem.content.value = value })
-            }
+            Text("Description:")
+            TextField(toDoItem.description.value, { value -> toDoItem.description.value = value })
             Button(onClick = {
-                saveFunction(toDoItem)
-                navController.popBackStack()
+                validationCheck(toDoItem)
+                if (validationViolations.isEmpty()) {
+                    saveFunction(toDoItem)
+                    navController.popBackStack()
+                }
             }) {
                 Text("Save")
             }
         }
+    }
+}
+
+fun validationCheck(toDoItem: ToDoItem) {
+    validationViolations.clear()
+
+    if (toDoItem.title.value.length < MIN_TITLE_LENGTH) {
+        validationViolations.add("Title must be at least 3 symbols long")
+    }
+    if (MAX_TITLE_LENGTH < toDoItem.title.value.length) {
+        validationViolations.add("Title must be at most 20 symbols long")
+    }
+    if (MAX_DESCRIPTION_LENGTH < toDoItem.description.value.length) {
+        validationViolations.add("Description must be at most 255 symbols long")
     }
 }
--- app/src/main/java/com/example/todolist/ToDoItem.kt
+++ app/src/main/java/com/example/todolist/ToDoItem.kt
@@ -1,21 +1,20 @@
 package com.example.todolist
 
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
 class ToDoItem {
     var isDone = mutableStateOf(false)
     var title = mutableStateOf("Unnamed to-do")
-    var content = mutableStateOf("To-do description")
+    var description = mutableStateOf("To-do description")
 
     constructor() {}
     constructor(title: String) {
         this.title.value = title
     }
-    constructor(isDone: Boolean, title: String, content: String) {
+    constructor(isDone: Boolean, title: String, description: String) {
         this.isDone.value = isDone
         this.title.value = title
-        this.content.value = content
+        this.description.value = description
     }
 
     fun toggleDone() {
@@ -23,6 +22,6 @@ class ToDoItem {
     }
 
     fun clone(): ToDoItem {
-        return ToDoItem(this.isDone.value, this.title.value, this.content.value)
+        return ToDoItem(this.isDone.value, this.title.value, this.description.value)
     }
 }
